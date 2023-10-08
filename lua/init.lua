063d283bd834f0ac9da18e428654cc2b2f0eb093
Richard <richard.prammer@itse.me>
2023-10-08 19:50:07 +0200

    add initial autosetup

--- lua/init.lua
+++ lua/init.lua
@@ -1,9 +1,10 @@
-local notes_core = require'notes_core'
+local notes_core = require'.notes_core'
+
+local default_opts = {main_location = vim.fn.expand('$HOME/notes')}
+notes_core.initialize_wiki(default_opts)
 
 local function setup(opts)
   opts = opts or {}
-  local default_opts = {main_location = vim.fn.expand('$HOME/notes')}
-
   opts = vim.tbl_extend('force', default_opts, opts)
   notes_core.initialize_wiki(opts)
 
